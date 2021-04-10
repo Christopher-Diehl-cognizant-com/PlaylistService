b2939855660485c6e442f6a247e46522362ed6e1
Mohammad G Javed <ghazanfar9131@gmail.com>
2021-04-10 11:52:51 -0400

    Unit Test:   -> PlayListControllerUnitTest: Refactored.

--- src/main/java/com/galvanize/playlist/response/CustomResponse.java
+++ src/main/java/com/galvanize/playlist/response/CustomResponse.java
@@ -3,10 +3,12 @@ package com.galvanize.playlist.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomResponse {
     private String message;
+    private HttpStatus status;
 }
